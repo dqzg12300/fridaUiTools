71895c137baa23be388735885cd5030712339abc
ajin <aasd1123>
2021-06-03 18:14:06 +0800

    init

--- kmainForm.py
+++ kmainForm.py
@@ -12,27 +12,90 @@ import os,re,zlib,hashlib,hmac
 class kmainForm(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.initUi()
+
+    def initUi(self):
         self.setWindowOpacity(0.93)
-        uic.loadUi("./ui/kmain.ui",self)
-        self.statusBar=QStatusBar()
+        uic.loadUi("./ui/kmain.ui", self)
+        self.statusBar = QStatusBar()
         self.labStatus = QLabel('当前状态:未连接')
         self.setStatusBar(self.statusBar)
         self.statusBar.addPermanentWidget(self.labStatus, stretch=1)
         self.actionattach.triggered.connect(self.actionAttach)
         self.actionabort.triggered.connect(self.actionAbort)
-        self.initUi()
-
-    def initUi(self):
         self.tabHooks.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-        # pass
+        self.btnShowClasses.clicked.connect(self.showClasses)
+        self.btnShowExport.clicked.connect(self.showExport)
+        self.btnShowStr.clicked.connect(self.showStr)
+        self.btnWallbreaker.clicked.connect(self.wallBreaker)
+        self.btnFindClassPath.clicked.connect(self.findClassPath)
+        self.btnShowMethods.clicked.connect(self.showMethods)
+        self.btnDumpPtr.clicked.connect(self.dumpPtr)
+        self.btnMatchDump.clicked.connect(self.matchDump)
 
     #打印输出日志
     def log(self,logstr):
         datestr = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S   ')
         self.txtLogs.appendPlainText(datestr+logstr)
-
+    #启动附加
     def actionAttach(self):
         self.log("actionAttach")
+    #是否附加进程了
+    def isattack(self):
+        if self.labStatus.text().index("未连接"):
+            return False
+        return True
+
+    #====================start======需要附加后才能使用的功能,基本都是在内存中查数据================================
+    def showClasses(self):
+        if self.isattack()==False:
+            self.log("Error:还未附加进程")
+            return
+        self.log("查询所有类")
+
+    def showExport(self):
+        if self.isattack()==False:
+            self.log("Error:还未附加进程")
+            return
+        self.log("查询so的所有符号")
+
+    def showStr(self):
+        if self.isattack() == False:
+            self.log("Error:还未附加进程")
+            return
+        self.log("将指定地址以str打印")
+
+    def wallBreaker(self):
+        if self.isattack() == False:
+            self.log("Error:还未附加进程")
+            return
+        self.log("wallBreaker功能")
+
+    def showMethods(self):
+        if self.isattack() == False:
+            self.log("Error:还未附加进程")
+            return
+        self.log("wallBreaker功能")
+
+    def findClassPath(self):
+        if self.isattack() == False:
+            self.log("Error:还未附加进程")
+            return
+        self.log("查找指定类的完整名称")
+
+    def dumpPtr(self):
+        if self.isattack() == False:
+            self.log("Error:还未附加进程")
+            return
+        self.log("dump指定地址")
+
+    def matchDump(self):
+        if self.isattack() == False:
+            self.log("Error:还未附加进程")
+            return
+        self.log("指定特征dump内存")
+    # ====================end======需要附加后才能使用的功能,基本都是在内存中查数据================================
+
 
     def actionAbort(self):
         QMessageBox().about(self, "About",
